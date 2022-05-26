25fc732bc7b16fa9a9c6306498309d82ef298c4e
admin <admin@DESKTOP-UJGJBG5>
2022-05-26 22:33:59 +0200

    init

---
+++ DI_Plantilla_Spring/src/main/java/com/example/demo/DiPlantillaSpringApplication.java
@@ -0,0 +1,13 @@
+package com.example.demo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DiPlantillaSpringApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DiPlantillaSpringApplication.class, args);
+	}
+
+}
